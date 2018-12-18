07e6f82a8e4cc542dc8fd9442117c0b0d2145958
Robert Ghilduta <robert.ghilduta@nuand.com>
2018-12-17 18:14:40 -0800

    libbladeRF: add OTP read/write support

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -3659,6 +3659,39 @@ int CALL_CONV bladerf_write_flash(struct bladerf *dev,
                                   uint32_t page,
                                   uint32_t count);
 
+
+/**
+ * Read data from the bladeRF's SPI flash OTP
+ *
+ * @note This function operates solely on the first 256 byte page of the OTP
+ *
+ * @param       dev     Device handle
+ * @param[in]   buf     Buffer to read OTP data into
+ *
+ * @return 0 on success,
+ *         or ::BLADERF_ERR_INVAL on an invalid `page` or `count` value,
+ *         or a value from \ref RETCODES list on other failures.
+ */
+API_EXPORT
+int CALL_CONV bladerf_read_otp(struct bladerf *dev,
+                                 uint8_t *buf);
+
+/**
+ * Write data to the bladeRF's SPI flash OTP device
+ *
+ * @note This function operates solely on the first 256 byte page of the OTP
+ *
+ * @param       dev     Device handle
+ * @param[in]   buf     Data to write to OTP
+ *
+ * @return 0 on success,
+ *         or ::BLADERF_ERR_INVAL on an invalid `page` or `count` value,
+ *         or a value from \ref RETCODES list on other failures.
+ */
+API_EXPORT
+int CALL_CONV bladerf_write_otp(struct bladerf *dev,
+                                  uint8_t *buf);
+
 /** @} (End of FN_SPI_FLASH) */
 
 /**
--- host/libraries/libbladeRF/src/bladerf.c
+++ host/libraries/libbladeRF/src/bladerf.c
@@ -1430,6 +1430,30 @@ int bladerf_write_flash(struct bladerf *dev,
     return status;
 }
 
+int bladerf_read_otp(struct bladerf *dev,
+                       uint8_t *buf)
+{
+    int status;
+    MUTEX_LOCK(&dev->lock);
+
+    status = dev->backend->get_otp(dev, (char *)buf);
+
+    MUTEX_UNLOCK(&dev->lock);
+    return status;
+}
+
+int bladerf_write_otp(struct bladerf *dev,
+                        uint8_t *buf)
+{
+    int status;
+    MUTEX_LOCK(&dev->lock);
+
+    status = dev->backend->write_otp(dev, (char *)buf);
+
+    MUTEX_UNLOCK(&dev->lock);
+    return status;
+}
+
 /******************************************************************************/
 /* Helpers & Miscellaneous */
 /******************************************************************************/
